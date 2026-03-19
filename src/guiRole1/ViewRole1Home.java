ef665e1193f1dab9283ca9998a46737f6e363a88
Brenna Coe <brenna.soto21@gmail.com>
2026-03-19 17:09:15 -0400

    Add ComboBox import to ViewRole1Home.java

--- src/guiRole1/ViewRole1Home.java
+++ src/guiRole1/ViewRole1Home.java
@@ -8,6 +8,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
