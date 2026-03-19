52779f909459a3ed9b00cbf95866ab55add34144
Brenna Coe <brenna.soto21@gmail.com>
2026-03-19 17:04:44 -0400

    Add imports for GUI components in ControllerRole2Home

--- src/guiRole2/ControllerRole2Home.java
+++ src/guiRole2/ControllerRole2Home.java
@@ -20,6 +20,11 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 /*******
  * <p> Title: ControllerRole2Home Class. </p>
